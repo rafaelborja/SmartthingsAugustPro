40886e5210b0040d945998fe2847876484637b99
Rafael Borja <5069188+rafaelborja@users.noreply.github.com>
2019-11-20 23:29:41 -0500

    First working version with child device
    - Removing old child devices when installing it (avoid generating waste device handlers)
    - Changed child device name
    - Removed device fingerprint for other locks
    - updateContactSensor method to simplify the logic to update events
    - updateChildContactDevice method to simplify update logic
    - Fixed log messages

--- devicetypes/rafaelborja/august-lock-pro-zwave-lock-with-doorsense.src/august-lock-pro-zwave-lock-with-doorsense.groovy
+++ devicetypes/rafaelborja/august-lock-pro-zwave-lock-with-doorsense.src/august-lock-pro-zwave-lock-with-doorsense.groovy
@@ -1,9 +1,11 @@
 /**
- *  August Lock Pro With Doorsense - Z-Wave Lock
+ *  August Lock Pro Z-Wave Lock With Doorsense - Z-Wave Lock
  *
  *  Copyright 2019 rafaelborja
  *  
  *  Z-Wave device handle for August Pro Smart Lock device handler with door sense.
+ *  Due to limitations (recognize contact sensor in automation and displaying information using tiles) this device handler creates a 
+ *  child contact sensor (based on https://github.com/SmartThingsCommunity/SmartThingsPublic/blob/master/devicetypes/smartthings/child-contact-sensor.src/child-contact-sensor.groovy)
  *
  *  Based on Smartthings "Z-Wave Lock" example from Smartthings New Device Handler wizard.
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
@@ -17,7 +19,7 @@
  *
  */
 metadata {
-	definition (name: "August Lock Pro Z-Wave Lock with Doorsense - With Child Contact Sensor", namespace: "rafaelborja", author: "Rafael Borja", runLocally: true, minHubCoreVersion: '000.017.0012',  executeCommandsLocally: false, genericHandler: "Z-Wave" , ocfDeviceType: "oic.d.smartlock", vid:"generic-lock" ) {
+	definition (name: "August Lock Pro Z-Wave Lock with Doorsense", namespace: "rafaelborja", author: "Rafael Borja", runLocally: true, minHubCoreVersion: '000.017.0012',  executeCommandsLocally: false, genericHandler: "Z-Wave" , ocfDeviceType: "oic.d.smartlock" , vid:"generic-lock" ) {
 		capability "Actuator"
 		capability "Lock"
 		capability "Polling"
@@ -30,53 +32,9 @@ metadata {
 
 		// Generic
 		fingerprint inClusters: "0x62, 0x63"
-		fingerprint deviceId: "0x4003", inClusters: "0x98"
-		fingerprint deviceId: "0x4004", inClusters: "0x98"
 		
 		// August Lock Pro
 		fingerprint mfr:"033F", prod:"0001", model:"0001", deviceJoinName: "August Smart Lock Pro"
-		
-		// KwikSet
-		fingerprint mfr:"0090", prod:"0001", model:"0236", deviceJoinName: "KwikSet SmartCode 910 Deadbolt Door Lock"
-		fingerprint mfr:"0090", prod:"0003", model:"0238", deviceJoinName: "KwikSet SmartCode 910 Deadbolt Door Lock"
-		fingerprint mfr:"0090", prod:"0001", model:"0001", deviceJoinName: "KwikSet SmartCode 910 Contemporary Deadbolt Door Lock"
-		fingerprint mfr:"0090", prod:"0003", model:"0339", deviceJoinName: "KwikSet SmartCode 912 Lever Door Lock"
-		fingerprint mfr:"0090", prod:"0003", model:"4006", deviceJoinName: "KwikSet SmartCode 914 Deadbolt Door Lock" //backlit version
-		fingerprint mfr:"0090", prod:"0003", model:"0440", deviceJoinName: "KwikSet SmartCode 914 Deadbolt Door Lock"
-		fingerprint mfr:"0090", prod:"0001", model:"0642", deviceJoinName: "KwikSet SmartCode 916 Touchscreen Deadbolt Door Lock"
-		fingerprint mfr:"0090", prod:"0003", model:"0642", deviceJoinName: "KwikSet SmartCode 916 Touchscreen Deadbolt Door Lock"
-		//zw:Fs type:4003 mfr:0090 prod:0003 model:0541 ver:4.79 zwv:4.34 lib:03 cc:5E,72,5A,98,73,7A sec:86,80,62,63,85,59,71,70,5D role:07 ff:8300 ui:8300
-		fingerprint mfr:"0090", prod:"0003", model:"0541", deviceJoinName: "KwikSet SmartCode 888 Touchpad Deadbolt Door Lock"
-		//zw:Fs type:4003 mfr:0090 prod:0003 model:0742 ver:4.10 zwv:4.34 lib:03 cc:5E,72,5A,98,73,7A sec:86,80,62,63,85,59,71,70,4E,8B,4C,5D role:07 ff:8300 ui:8300
-		fingerprint mfr:"0090", prod:"0003", model:"0742", deviceJoinName: "Kwikset Obsidian Lock"
-		// Schlage
-		fingerprint mfr:"003B", prod:"6341", model:"0544", deviceJoinName: "Schlage Touchscreen Deadbolt Door Lock"
-		fingerprint mfr:"003B", prod:"6341", model:"5044", deviceJoinName: "Schlage Touchscreen Deadbolt Door Lock"
-		fingerprint mfr:"003B", prod:"634B", model:"504C", deviceJoinName: "Schlage Connected Keypad Lever Door Lock"
-		fingerprint mfr:"003B", prod:"0001", model:"0468", deviceJoinName: "Schlage Connect Smart Deadbolt Door Lock" //BE468ZP
-		fingerprint mfr:"003B", prod:"0001", model:"0469", deviceJoinName: "Schlage Connect Smart Deadbolt Door Lock" //BE469ZP
-		// Yale
-		fingerprint mfr:"0129", prod:"0002", model:"0800", deviceJoinName: "Yale Touchscreen Deadbolt Door Lock" // YRD120
-		fingerprint mfr:"0129", prod:"0002", model:"0000", deviceJoinName: "Yale Touchscreen Deadbolt Door Lock" // YRD220, YRD240
-		fingerprint mfr:"0129", prod:"0002", model:"FFFF", deviceJoinName: "Yale Touchscreen Lever Door Lock" // YRD220
-		fingerprint mfr:"0129", prod:"0004", model:"0800", deviceJoinName: "Yale Push Button Deadbolt Door Lock" // YRD110
-		fingerprint mfr:"0129", prod:"0004", model:"0000", deviceJoinName: "Yale Push Button Deadbolt Door Lock" // YRD210
-		fingerprint mfr:"0129", prod:"0001", model:"0000", deviceJoinName: "Yale Push Button Lever Door Lock" // YRD210
-		fingerprint mfr:"0129", prod:"8002", model:"0600", deviceJoinName: "Yale Assure Lock" //YRD416, YRD426, YRD446
-		fingerprint mfr:"0129", prod:"0007", model:"0001", deviceJoinName: "Yale Keyless Connected Smart Door Lock"
-		fingerprint mfr:"0129", prod:"8004", model:"0600", deviceJoinName: "Yale Assure Lock Push Button Deadbolt" //YRD216
-		fingerprint mfr:"0129", prod:"6600", model:"0002", deviceJoinName: "Yale Conexis Lock"
-		fingerprint mfr:"0129", prod:"0001", model:"0409", deviceJoinName: "Yale Touchscreen Lever Door Lock" // YRL-220-ZW-605
-		fingerprint mfr:"0129", prod:"800B", model:"0F00", deviceJoinName: "Yale Assure Keypad Lever Door Lock" // YRL216-ZW2
-		fingerprint mfr:"0129", prod:"800C", model:"0F00", deviceJoinName: "Yale Assure Touchscreen Lever Door Lock" // YRL226-ZW2
-		fingerprint mfr:"0129", prod:"8002", model:"1000", deviceJoinName: "Yale Assure Lock" //YRD-ZWM-1
-		// Samsung
-		fingerprint mfr:"022E", prod:"0001", model:"0001", deviceJoinName: "Samsung Digital Lock", mnmn: "SmartThings", vid: "SmartThings-smartthings-Samsung_Smart_Doorlock" // SHP-DS705, SHP-DHP728, SHP-DHP525
-		// KeyWe
-		fingerprint mfr:"037B", prod:"0002", model:"0001", deviceJoinName: "KeyWe Lock" // GKW-2000D
-		fingerprint mfr:"037B", prod:"0003", model:"0001", deviceJoinName: "KeyWe Smart Rim Lock" // GKW-1000Z
-		// Philia
-		fingerprint mfr:"0366", prod:"0001", model:"0001", deviceJoinName: "Philia Smart Door Lock" // PDS-100
 	}
 
 	simulator {
@@ -132,11 +90,11 @@ def installed() {
 	// Device-Watch pings if no device events received for 1 hour (checkInterval)
 	sendEvent(name: "checkInterval", value: 1 * 60 * 60, displayed: false, data: [protocol: "zwave", hubHardwareId: device.hub.hardwareID, offlinePingable: "1"])
 	
-    createChildDevices()
+    createContactChildDevice()
     
 	scheduleInstalledCheck()
-    
 }
+
 
 /**
  * Verify that we have actually received the lock's initial states.
@@ -169,7 +127,8 @@ def installedCheck() {
  */
 def uninstalled() {
 	def deviceName = device.displayName
-	log.debug "[DTH] Executing 'uninstalled()' for device $deviceName"
+	log.debug "[DTH] Executing 'uninstalled()' for device ${device.displayName}"
+    removeChildDevices(getChildDevices())
 	sendEvent(name: "lockRemoved", value: device.id, isStateChange: true, displayed: false)
 }
 
@@ -371,15 +330,13 @@ def zwaveEvent(DoorLockOperationReport cmd) {
 		map.descriptionText = cmd.doorCondition >> 1 ? "Unlocked" : "Locked"
 	} else if (cmd.doorLockMode == 0xFF) {
         // TODO rafaeb (add this event to return method
-        log.debug "RAFEB doorCondition is $cmd.doorCondition"
+        log.debug "doorCondition is $cmd.doorCondition"
         if (cmd.doorCondition == 0x01 || cmd.doorCondition == 0x00) {
              log.debug "Door is closed"
-             sendEvent(name: "contact", value: "closed", descriptionText: "Lock is closed")
-             updateChild("closed")
+             updateContactSensor("closed")
         } else {
              log.debug "Door is open"
-             sendEvent(name: "contact", value: "open", descriptionText: "Lock is open")
-             updateChild("open")
+             updateContactSensor("open")
         }
 		map.value = "locked"
 		map.descriptionText = "Locked"
@@ -397,13 +354,11 @@ def zwaveEvent(DoorLockOperationReport cmd) {
        	   // 0x01 for open
            if (cmd.doorCondition == 0x01 || cmd.doorCondition == 0x00 || cmd.doorCondition == 0x03) {
              log.debug "Door is closed"
-             sendEvent(name: "contact", value: "closed", descriptionText: "Lock is closed")
-             updateChild("closed")
+             updateContactSensor("closed")
            } else {
              log.debug "Door is open"
              // 0x02
-             sendEvent(name: "contact", value: "open", descriptionText: "Lock is open")
-             updateChild("open")
+             updateContactSensor("open")
            }
        }
 
@@ -1841,30 +1796,56 @@ def zwaveEvent(physicalgraph.zwave.commands.sensorbinaryv1.SensorBinaryReport cm
 
 
 
-private void createChildDevices() {
-	// state.oldLabel = device.label
-
-	def i = 1
+private createContactChildDevice() {
+	log.debug "[DTH] Executing 'createContactChildDevice()' for device ${device.displayName}"
+    
+    log.debug "[DTH] Removing existing child devices ${getChildDevices()}"
+    removeChildDevices(getChildDevices())
+    
+    log.debug "[DTH] Adding contact child device"
 	def child = addChildDevice("Child Contact Sensor",
-				"${device.deviceNetworkId}:$i", // TODO ADD CONSTANT
+				"${device.deviceNetworkId}:contact",
 				device.hubId,
 				[completedSetup: true,
-				 label: "${device.displayName} contact sensor $i",
-				 isComponent: true,
-				 componentName: "contact$i",
-				 componentLabel: "contact $i"])
-
-    updateChild("closed")
+				 label: "${device.displayName} door contact sensor",
+				 isComponent: false,
+				 componentName: "contactsensor",
+				 componentLabel: "door contact sensor"])
+	
+    log.debug "[DTH] Contact child device added"
+    
+    updateChildContactDevice(contact? contact: "closed")
 }
 
-private updateChild(value) {
+private removeChildDevices(delete) {
+    delete.each {
+        deleteChildDevice(it.deviceNetworkId)
+    }
+}
+
+/**
+  * Updates door sense contact value.
+  * @param value: "open" or "close"
+  */
+private updateContactSensor(value) {
+	log.debug "[DTH] Updating contact sensor to ${value}"
+	sendEvent(name: "contact", value: value, descriptionText: "Door is ${value}", displayed: true, isStateChange: contact != value)
+    updateChildContactDevice(value)
+}
+
+/**
+  * Updates child contact sensor device with door sense contact value.
+  * @param value: "open" or "close"
+  */
+private updateChildContactDevice(value) {
+	log.debug "[DTH] Updating child contact sensor to ${value}"
     
-    String childDni = "${device.deviceNetworkId}:1" // TODO ADD CONSTANT
-    def child = childDevices.find{it.deviceNetworkId == childDni}
+	String childDni = "${device.deviceNetworkId}:contact"
+	def child = childDevices.find{it.deviceNetworkId == childDni}
     if (!child) {
         log.error "Child device $childDni not found"
     } else {
-        log.debug "Sending event to child device $childDni"
-        child?.sendEvent(name: "contact", value: value, descriptionText: "Door is $value", displayed: true, isStateChange: true) // TODO isStateChange dependent on initial value
-    } 
+    	log.debug "Sending update to child ${child.displayName} (deviceNetworkId ${child.deviceNetworkId}) value: $value"
+    	child.sendEvent(name: "contact", value: value, descriptionText: "Door is $value", displayed: true, isStateChange: contact != value)
+    }
 }
